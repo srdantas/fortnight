f68c144e817777697b0c0374206045ab843be60e
Renato VIeira Dantas <renato.srdantas@gmail.com>
2020-07-23 20:20:17 -0300

    configure app

---
+++ app/src/test/java/com/fortnight/FortnightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fortnight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortnightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
